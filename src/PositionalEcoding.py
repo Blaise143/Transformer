8589a32ab5032c1ab7a596394181972a96a26368
Blaise143 <blaiseappo@gmail.com>
2023-08-28 22:49:33 -0600

    implemented positional encodings

--- src/PositionalEcoding.py
+++ src/PositionalEcoding.py
@@ -1,7 +1,37 @@
 import torch
-import torch.nn as nn
+import math
+import matplotlib.pyplot as plt
 
 
-class PositionalEncoding(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
+class PositionalEncoding():
+    def __init__(self, dim: int, max_position: int) -> None:
+        """Implements a sinusoidal positional encoding
+
+        Args:
+            dim (int): The dimension of the input embeddings
+            max_position (int): The maximun number of positions (the number of embeddings)
+        """
+        self.dim = dim
+        self.max_position = max_position
+
+    def generate(self) -> torch.tensor:
+        """Generates the positional encoding
+
+        Returns:
+            torch.tensor: The positional encodings genetated
+        """
+        positions = torch.arange(0, self.max_position).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, self.dim, 2)
+                             * - math.log(10_000) / self.dim)
+        sin_embedding = torch.sin(positions*div_term)
+        cos_embedding = torch.cos(positions*div_term)
+
+        embedding = torch.stack((sin_embedding, cos_embedding), dim=2)
+        embedding = embedding.view(self.max_position, -1)
+        return embedding
+
+
+if __name__ == "__main__":
+    p_e = PositionalEncoding(2000, 2000)
+    plt.matshow(p_e.generate())
+    plt.show()
